184fd4bc5a834aa3ea3bc385b6caec80ab214416
Ermishina Evgeniya <evermishka@yandex.ru>
2025-08-01 14:43:24 +0300

    Добавляет useToggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-function App() {
+import { useToggle } from "./hooks/use-toggle";
 
-  return (
-      <p>React tasks</p>
-  )
+function App() {
+    const [value, toggle] = useToggle(['blue', 'orange', 'cyan', 'teal']);
+
+    return (
+        <button onClick={() => toggle()}>
+            {value}
+        </button>
+    );
 }
 
 export default App
