f1f9114c2c73831655c2376705f46c90867e76f3
Ermishina Evgeniya <evermishka@yandex.ru>
2025-08-03 18:01:16 +0300

    Добавляет компонент Signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import { Signin, type SigninData } from "./components";
+import { Signin, Signup } from "./components";
+import type { FormData } from "./hooks";
 
 function App() {
+    const handleSininSubmit = (data: FormData) => {
+        console.log('SigninData', data)
+    }
 
-  const handleSininSubmit = (data: SigninData) => {
-    console.log('SigninData', data)
-  }
+    const handleSinupSubmit = (data: FormData) => {
+       console.log('SignupData', data)
+    }
 
-  return (
-    <>
-      <Signin onSubmit={handleSininSubmit}/>
-    </>
-  )
+    return (
+        <>
+            <Signin onSubmit={handleSininSubmit}/>
+            <Signup onSubmit={handleSinupSubmit}/>
+        </>
+    )
 }
 
 export default App
