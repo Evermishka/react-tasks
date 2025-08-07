0292bbbd7737149f2b8193a04ca472cb7c454bd5
Ermishina Evgeniya <evermishka@yandex.ru>
2025-08-07 11:18:11 +0300

    Добавляет типизацию для WindowScroll

--- src/hooks/use-window-scroll.ts
+++ src/hooks/use-window-scroll.ts
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import { useWindowEvent } from "./use-window-event";
 
+type WindowScroll = {
+    x: number;
+    y: number;
+};
+
 export const useWindowScroll = () => {
-    const [scroll, setScroll] = useState({ x: window.scrollX, y: window.scrollY });
+    const [scroll, setScroll] = useState<WindowScroll>({ x: window.scrollX, y: window.scrollY });
 
     const scrollTo = ({ 
         x = scroll.x, y = scroll.y 
