78c72cf23c8d615122808d0f7789235e52b8ea69
Ermishina Evgeniya <evermishka@yandex.ru>
2025-08-07 12:13:27 +0300

    Упрощает реализацию reducer в useToggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { useToggle } from "./hooks/use-toggle";
 
 function App() {
-    const [value, toggle] = useToggle(['blue', 'orange', 'cyan', 'teal']);
+    const [option, toggle] = useToggle(['blue', 'orange', 'cyan', 'teal']);
 
     return (
-        <button onClick={() => toggle()}>
-            {value}
+        <button onClick={() => toggle((option) => !option)}>
+            {option}
         </button>
     );
 }
--- src/hooks/use-toggle.ts
+++ src/hooks/use-toggle.ts
@@ -1,34 +1,17 @@
 import { useReducer } from "react";
 
-type Action = 
-  | { type: "TOGGLE" }
-  | { type: "SET_VALUE"; payload: string };
+type ActionFn = (val: string | boolean) => string | boolean;
+type Action = string | ActionFn;
 
-const reducer = (state: string | boolean, action: Action) => {
-    switch (action.type) {
-        case "TOGGLE":
-            return !state;
-        case "SET_VALUE":
-            return action.payload;
-        default:
-            return state;
-    }
-};
+export const useToggle = (options: (string | boolean)[] = [false, true]) => {
+    const [[option], toggle] = useReducer((state, action: Action) => {
+    const value = typeof action === "function" ? 
+        action(state[0]) : 
+        action;
+    const index = Math.abs(state.indexOf(value));
 
-export const useToggle = (values: string[] = []) => {
-    const [value, dispatch] = useReducer(reducer, values.length > 0 ? values[0] : false);
+    return state.slice(index).concat(state.slice(0, index));
+  }, options);
 
-    const toggle = (newValue = '') => {
-        if (values.length > 0 && newValue) {
-            dispatch({ type: "SET_VALUE", payload: newValue });
-        } else if (values.length > 0) {
-            const currentIndex = values.indexOf(value as string);
-            const nextIndex = (currentIndex + 1) % values.length;
-            dispatch({ type: "SET_VALUE", payload: values[nextIndex] });;
-        } else {
-            dispatch({ type: "TOGGLE" });
-        }       
-    };
-
-    return [value, toggle] as const;
+  return [option, toggle] as const;
 };
