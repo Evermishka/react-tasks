88444a3198c4d2636d5852197a9816c8b56e1990
Ermishina Evgeniya <evermishka@yandex.ru>
2025-08-07 11:27:02 +0300

    Добавляет установку начального значения scroll после монтирования компонента

--- src/hooks/use-window-scroll.ts
+++ src/hooks/use-window-scroll.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useWindowEvent } from "./use-window-event";
 
 type WindowScroll = {
@@ -10,6 +10,10 @@ type ScrollTo = (windowScroll: WindowScroll) => void;
 
 export const useWindowScroll = () => {
     const [scroll, setScroll] = useState<WindowScroll>({ x: window.scrollX, y: window.scrollY });
+
+    useEffect(() => {
+        setScroll({ x: window.scrollX, y: window.scrollY });
+    }, []);
 
     const scrollTo = useCallback<ScrollTo>(({ 
         x = scroll.x, y = scroll.y 
